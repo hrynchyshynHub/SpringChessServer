5fc01d6afc60de9c223807d6117538fd6a251882
Ivan Hrynchyshyn <ivan.hrynchyshyn@eleks.com>
2018-03-09 14:33:36 +0200

    Fixed problem with entityManager bean.

--- server/src/main/java/com/chess/Chess/ChessApplication.java
+++ server/src/main/java/com/chess/Chess/ChessApplication.java
@@ -3,8 +3,10 @@ package com.chess.Chess;
 import com.chess.Chess.server.Server;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ChessApplication {
 
 	public static void main(String[] args) {

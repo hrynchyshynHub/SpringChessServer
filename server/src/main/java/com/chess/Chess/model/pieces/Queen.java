ca1db632e573d8414dcd20c872ffa4c617579353
hrynchyshynHub <vaniahrynchyshyn@gmail.com>
2018-03-10 19:51:23 +0200

    Implemented Queen availbles move

--- server/src/main/java/com/chess/Chess/model/pieces/Queen.java
+++ server/src/main/java/com/chess/Chess/model/pieces/Queen.java
@@ -25,7 +25,132 @@ public class Queen extends Piece {
 
     @Override
     public List<String> getAvailableCellsToMove(Board board) {
-        return null;
+        availableCellsToMove.clear();
+        Cell currentCell = getCurrentCell();
+        int xPos = currentCell.getX();
+        char yPos = currentCell.getY();
+        int step = 0;
+        for (int i = xPos + 1; i <= 8; i++) {
+            step++;
+            String rightUpCell = new String(i + "" + (char) (yPos + step));
+            if (board.getCellById(rightUpCell) != null){
+                Piece piece = board.getCellById(rightUpCell).getPiece();
+                if(piece == null ) {
+                    availableCellsToMove.add(rightUpCell);
+                }else{
+                    if(piece.getColor() != getColor()){
+                        availableCellsToMove.add(rightUpCell);
+                        break;
+                    }else
+                        break;
+                }
+            }
+        }
+
+        for (int i = xPos + 1; i <= 8; i++) {
+            step++;
+            String leftUpCell = new String(i + "" + (char) (yPos - step));
+            if (board.getCellById(leftUpCell) != null){
+                Piece piece = board.getCellById(leftUpCell).getPiece();
+                if(piece == null ) {
+                    availableCellsToMove.add(leftUpCell);
+                }else{
+                    if(piece.getColor() != getColor()){
+                        availableCellsToMove.add(leftUpCell);
+                        break;
+                    }else
+                        break;
+                }
+            }
+        }
+
+        step = 0;
+
+        for(int c = xPos - 1 ; c >= 1; c--){
+            step++;
+            String rightDownCell = new String(c + "" + (char) (yPos + step));
+            if (board.getCellById(rightDownCell) != null){
+                Piece piece = board.getCellById(rightDownCell).getPiece();
+                if(piece == null ) {
+                    availableCellsToMove.add(rightDownCell);
+                }else{
+                    if(piece.getColor() != getColor()){
+                        availableCellsToMove.add(rightDownCell);
+                        break;
+                    }else
+                        break;
+                }
+            }
+        }
+        for(int c = xPos - 1 ; c >= 1; c--){
+            step++;
+            String leftDownCell = new String(c + "" + (char) (yPos - step));
+            if (board.getCellById(leftDownCell) != null){
+                Piece piece = board.getCellById(leftDownCell).getPiece();
+                if(piece == null ) {
+                    availableCellsToMove.add(leftDownCell);
+                }else{
+                    if(piece.getColor() != getColor()){
+                        availableCellsToMove.add(leftDownCell);
+                        break;
+                    }else
+                        break;
+                }
+            }
+        }
+
+        for(int i = xPos; i <= 8 ; i++){
+            Piece piece = board.getCellById(new String(i + "" + yPos)).getPiece();
+            if(piece == null){
+                availableCellsToMove.add(new String(i+""+yPos));
+            }else{
+                if(piece.getColor() != getColor()){
+                    availableCellsToMove.add(new String(i+""+yPos));
+                    break;
+                }else{
+                    break;
+                }
+            }
+        }
+        for(int i = xPos; i > 0 ; i--){
+            Piece piece = board.getCellById(new String(i + "" + yPos)).getPiece();
+            if(piece == null){
+                availableCellsToMove.add(new String(i+""+yPos));
+            }else{
+                if (piece.getColor() != getColor()){
+                    availableCellsToMove.add(new String(i+""+yPos));
+                    break;
+                }else
+                    break;
+            }
+        }
+
+        for (char a = yPos; a <= 'h'; a++) {
+            Piece piece = board.getCellById(new String(xPos + "" + a)).getPiece();
+            if (piece == null) {
+                availableCellsToMove.add(new String(xPos + "" + a));
+            } else {
+                if (piece.getColor() != getColor()) {
+                    availableCellsToMove.add(new String(xPos + "" + a));
+                    break;
+                } else
+                    break;
+            }
+        }
+        for(char a = yPos ; a > 'a'; a--){
+            Piece piece = board.getCellById(new String(xPos + "" + a)).getPiece();
+            if(piece == null){
+                availableCellsToMove.add(new String(xPos+""+a));
+            }else{
+                if(piece.getColor() != getColor()){
+                    availableCellsToMove.add(new String(xPos+""+a));
+                    break;
+                }else{
+                    break;
+                }
+            }
+        }
+        return availableCellsToMove;
     }
 
     public Deque<Cell> getDefaultCellStack() {
@@ -36,4 +161,5 @@ public class Queen extends Piece {
     public String toString() {
         return "Queen";
     }
+
 }
